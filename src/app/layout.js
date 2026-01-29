3fe582ad283228902bdb088902dfdcb6a6419d9a
Aniket Shelake <aniketshelake151@sitcoe.org.in>
2026-01-29 12:38:31 +0530

    add google verification code

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,12 @@ const montserrat = Montserrat({ subsets: ["latin"], weight: ["600", "700"] });
 
 export const metadata = {
   title: "Krishigyan AI",
-  description: "Krishigyan AI collaborate closely with farmers, agronomists, and industry experts to understand the unique challenges they face. Through these partnerships, we co-create solutions and deliver cutting-edge technology tailored to their specific needs.",
+  description:
+    "Krishigyan AI collaborate closely with farmers, agronomists, and industry experts to understand the unique challenges they face. Through these partnerships, we co-create solutions and deliver cutting-edge technology tailored to their specific needs.",
+
+  verification: {
+    google: "0C4ZNqEgoXMk0WeRU0gnwmRRve0Rh3GJ8hxvBEu7Olo",
+  },
 };
 
 export default function RootLayout({ children }) {
